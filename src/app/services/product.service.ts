41fffdaab9cf6087309ee2942c5f7bfaac1e5175
VladNohai <vladnohai@gmail.com>
2022-07-12 18:36:36 +0300

    create login page and navbar

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,10 @@
+import { Product } from "../product/product.component";
+
+export class ProductService{
+    products : Product[] = [{
+            id : 0,
+            title : "test",
+            description : "descriere",
+            price : 15
+    }]
+}

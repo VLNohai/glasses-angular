712c94c04ffc9c75968b1bf5cf755d314a657c6b
VladNohai <vladnohai@gmail.com>
2022-07-17 19:57:55 +0300

    calculate total in shopping cart and some styling changes

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,6 +14,24 @@ export class ProductService{
         title : "test2",
         description : "descriere2",
         price : 20
+    },
+    {
+        id : 2,
+        title : "test3",
+        description : "descriere3",
+        price : 10
+    },
+    {
+        id : 3,
+        title : "test4",
+        description : "descriere4",
+        price : 25
+    },
+    {
+        id : 4,
+        title : "test5",
+        description : "descriere5",
+        price : 7
     }
 ]
     getProducts(){
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -10,17 +10,28 @@ import { ShoppingService } from '../services/shopping.service';
 export class ShoppingCartComponent implements OnInit {
 
   productMap = new Map<Product, number>()
+  totalPrice : number = 0;
 
   constructor(private shoppingService : ShoppingService) { 
     this.productMap = shoppingService.getProducts();
+    this.calculateTotal();
   }
 
   removeOneItem(product : Product){
     if(this.productMap.get(product)! > 1){
       this.productMap.set(product, this.productMap.get(product)! - 1);
-      return;
     }
+    else{
     this.productMap.delete(product);
+    }
+    this.calculateTotal();
+  }
+
+  calculateTotal(){
+    this.totalPrice = 0;
+    for(let product of this.productMap.keys()){
+      this.totalPrice += product.price * this.productMap.get(product)!;
+    }
   }
 
   ngOnInit(): void {
